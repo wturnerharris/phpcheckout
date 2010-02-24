ff7eebe4faabec51ef5228444ef72b35a052713f
wturnerharris <wturnerharris@2ce709ec-1cee-11df-959f-133e3f6a5118>
2010-02-24 18:16:49 +0000

    * styled result page for fines action * styled link for kithistory icon on allequipmentbyclass * added return link to kithistory

--- equipment/finesaction.php
+++ equipment/finesaction.php
@@ -12,7 +12,12 @@ mysql_query($sql, $equip) or die(mysql_error());
 
 ?>
 
-Fine Paid. Thank you. 
+<meta http-equiv="refresh" content="1,../studentinfo.php?StudentID=<?php echo $StudentID; ?>" />
+<div id="alert" style="visibility:visible">
+Fines Paid<br />
+<br />
+Returning to Student: <? echo $StudentID ?><br />
+</div>
 
 <? 
 include 'includes/footer.html';
