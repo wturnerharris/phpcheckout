75b8790f8f35c000b82a310f834d6aceb80804a2
wturnerharris <wturnerharris@2ce709ec-1cee-11df-959f-133e3f6a5118>
2010-03-08 06:32:38 +0000

    * finalized changes to strike/fines - should be operational * began adding functionality to equipment admin

--- equipment/admin/equipment.php
+++ equipment/admin/equipment.php
@@ -1,15 +1,24 @@
-<?php 
+<?php
 
 //variables
+$EquipmentID = $_REQUEST['equipmentList'];
+$hideButtons = false;
+
+if ($_REQUEST['addEquipment'] == "true") {
+	$addEquipment = true;
+    $hideButtons = true;
+}
+if ($_REQUEST['addAccessory'] == "true") {
+	$addAccessory = true;
+    $hideButtons = true;
+}
 $filter = $_REQUEST['filter'];
-if (isset($filter)) {
-} else {
+if (!isset($filter)) {
 	$filter = "no";
 }
-
 $filterBy = $_REQUEST['filterList'];
 if ($filterBy  == "no") { 
-	$filter = "no"; 
+	$filter = "no";
 		}
 //global javascript
 ?>
@@ -18,10 +27,25 @@ if ($filterBy  == "no") {
 function Filter(){
 	$('filter').value = "yes";
 //	alert('Submit Called');
+	submitForm1();
+}
+function submitForm1() {
 	$('form1').submit();
 }
-
-</script>		
+function showAddEquipment() {
+	$('addEquipment').value = "true";
+	submitForm1();
+}
+function showAddAccessory() {
+	$('addAccessory').value = "true";
+	submitForm1();
+}
+function refreshPage(){
+	$('filter').value = "no";
+	var location = "admin.php?page=equipment"
+	window.location.href = location;
+}
+</script>
 
 <form id="form1" name="form1" action="admin.php?page=equipment" method="post">
 	<p><strong style="line-height: 30px;">Genre: </strong>
@@ -35,12 +59,13 @@ function Filter(){
 		<option <?php if ($filterBy == "Video Camera") { echo "selected"; } ?> value="Video Camera">Video Camera</option>
 	</select>
 	<input id="btn" type="button" style="margin-left: 30px; height: 25px; width: 65px;" onClick="Filter();" value="Filter" onKeyPress="Filter();" />
-
 	</p><hr/ style="border: 0px; height: 3px; background-color: #ffcc00;">
 	<strong style="line-height: 30px;">Equipment: </strong>
 	
+	<input type="hidden" name="addEquipment" id="addEquipment" value="">
+	<input type="hidden" name="addAccessory" id="addAccessory" value="">
 	<input id="filter" name="filter" type="hidden" value="<?php echo $filter; ?>" />
-	<select name="equipmentList" id="equipmentList" style="float: right; width: 300px; height: 25px; margin-right: 35px; margin-top: 5px;" onChange="submitForm();">
+	<select name="equipmentList" id="equipmentList" style="float: right; width: 300px; height: 25px; margin-right: 35px; margin-top: 5px;" onChange="submitForm1();">
 		<option selected value="">Select the equipment...</option>
 
 <?php
@@ -81,67 +106,80 @@ while($loop_Equipment = mysql_fetch_assoc($Equipment)) {
 ?>
 <br>
 <?php
-if ($filter == "yesyes") {
-?>
+if (empty($EquipmentID)) { if (!$hideButtons) { ?>
+<div style="margin-left: auto; margin-right: auto;">
+<a href="#" title="Add Equipment" onClick="showAddEquipment();"><img src="<?php echo $root; ?>/images/btn-add-equipment.png" border="0" /></a>
+<a href="#" title="Add Accessories" onClick="showAddAccessory();"><img src="<?php echo $root; ?>/images/btn-add-accessories.png" border="0" /></a>
+</div>
+<? }} else { ?>
 <div id="showModify">
-<form id="form2" name="form2" action="#" method="post">
-<strong>Name: </strong>
-<input type="textarea" id="txtStudentID" name="txtStudentID" value="<?php echo $row_Students['StudentID']; ?>" readonly="true" onClick="this.readOnly = false; this.style.background = '#ffffff'; this.style.color = '#000000'; EnableButton();" onKeyPress="return disableEnterKey(event)"/>
-<br />
-<strong>Image: </strong>
-<input type="textarea" id="txtFirstName" name="txtFirstName" value="<?php echo $row_Students['FirstName']; ?>" readonly="true" onClick="this.readOnly = false; this.style.background = '#ffffff'; this.style.color = '#000000'; EnableButton();" onKeyPress="return disableEnterKey(event)"/>
-<br />
-<strong>Genre: </strong>
-<input type="textarea" id="txtLastName" name="txtLastName" value="<?php echo $row_Students['LastName']; ?>" readonly="true" onClick="this.readOnly = false; this.style.background = '#ffffff'; this.style.color = '#000000'; EnableButton();" onKeyPress="return disableEnterKey(event)"/>
-<br />
-<strong>Serial: </strong>
-<input type="textarea" id="txtEmail" name="txtEmail" value="<?php echo $row_Students['Email']; ?>" readonly="true" onClick="this.readOnly = false; this.style.background = '#ffffff'; this.style.color = '#000000'; EnableButton();" onKeyPress="return disableEnterKey(event)"/>
-<br />
-<strong>Model: </strong>
-<input type="textarea" id="txtPhone" name="txtPhone" value="<?php echo $row_Students['Phone']; ?>" readonly="true" onClick="this.readOnly = false; this.style.background = '#ffffff'; this.style.color = '#000000'; EnableButton();" onKeyPress="return disableEnterKey(event)"/>
-<br />
-<br />
-<a href="#" style="float: right; margin-right: 35px;" onClick="Modify()">
-	<img src="<?php echo $root; ?>/images/modify-button.png" border="0" title="Modify" /></a>
-<a href="#" style="float: right; margin-right: 10px;" onClick="refreshPage()">
-	<img src="<?php echo $root; ?>/images/cancel-button.png" border="0" title="Cancel" /></a>
-<a href="#" style="float: right; margin-right: 10px;" onClick="answer=confirm('Do you wish to remove <?php echo $row_Students["FirstName"]; ?> <?php echo $row_Students["LastName"]; ?> from the student records?');if(answer!=0){delEntry();}else{alert('Canceled')}">
-	<img src="<?php echo $root; ?>/images/remove-button.png" border="0" title="Remove" /></a>
-<input id="pID" name="pID" type="hidden" value="<?php echo $row_Students['ID']; ?>" />
+<form id="frmModEquip" name="frmModEquip" enctype="multipart/form-data" action="actions-equipment.php" method="POST">
+	<input type="hidden" name="EquipmentID" value="<? echo $EquipmentID; ?>" />
+	<label>Name: </label><input type="text" name="textfield" /><br>
+	<label>Image: </label><input type="file" /><br>
+	<label>Genre: </label><input type="text" name="textfield4" /><br>
+	<label>Check Hours: </label><input type="text" name="textfield5" /><br>
+	<label>Serial #: </label><input type="text" name="textfield6" /><br>
+	<label>Model #: </label><input type="text" name="textfield7" /><br>
+	<label>Thumbnail: </label><input type="file" /><br>
+	<label>Needs Repair: </label><input type="checkbox" name="checkbox2" value="checkbox" /><label>Special Contract Required: </label><input type="checkbox" name="checkbox" value="checkbox" /><br>
+	<label>Notes: </label><textarea name="textarea"></textarea>
+	<input type="hidden" name="MAX_FILE_SIZE" value="500" />
+	<br />
+	<a href="#" style="float: right; margin-right: 35px;" onClick="Modify()">
+		<img src="<?php echo $root; ?>/images/modify-button.png" border="0" title="Modify" /></a>
+	<a href="#" style="float: right; margin-right: 10px;" onClick="refreshPage()">
+		<img src="<?php echo $root; ?>/images/cancel-button.png" border="0" title="Cancel" /></a>
+	<a href="#" style="float: right; margin-right: 10px;" onClick="answer=confirm('Do you wish to remove this record?');if(answer!=0){delEntry();}else{alert('Canceled')}">
+		<img src="<?php echo $root; ?>/images/remove-button.png" border="0" title="Remove" /></a>
+</form>
+</div>
+<?
+}
+if ($addEquipment) { ?>
+<div id="showAddEquip">
+<form id="frmAddEquip" name="frmAddEquip" enctype="multipart/form-data" action="actions-equipment.php" method="POST">
+	<label>Name: </label><input type="text" name="textfield" />
+	<label>Image: </label><input type="file" />
+	<label>Genre: </label><input type="text" name="textfield4" />
+	<label>Check Hours: </label><input type="text" name="textfield5" />
+	<label>Serial #: </label><input type="text" name="textfield6" />
+	<label>Model #: </label><input type="text" name="textfield7" />
+	<label>Thumbnail: </label><input type="file" />
+	<label>Needs Repair: </label><input type="checkbox" name="checkbox2" value="checkbox" /><label>Special Contract Required: </label><input type="checkbox" name="checkbox" value="checkbox" />
+	<label>Notes: </label><textarea name="textarea"></textarea>
+	<input type="hidden" name="MAX_FILE_SIZE" value="500" />
+	<br />
+	<a href="#" style="float: right; margin-right: 35px;" onClick="Add()">
+		<img src="<?php echo $root; ?>/images/add-button.png" border="0" title="Add" /></a>
+	<a href="#" style="float: right; margin-right: 10px;" onClick="refreshPage()">
+		<img src="<?php echo $root; ?>/images/cancel-button.png" border="0" title="Cancel" /></a>
 </form>
 </div>
 <?php
-} else {
-if ($filter == "add") {
-?>
-<div id="showAdd">
-<form id="form3" name="form3" action="#" method="post">
-<strong>Name: </strong>
-<input type="textarea" id="txtStudentID1" name="txtStudentID1" value="" />
-<br />
-<strong>Image: </strong>
-<input type="textarea" id="txtFirstName1" name="txtFirstName1" value="" />
-<br />
-<strong>Genre: </strong>
-<input type="textarea" id="txtLastName1" name="txtLastName1" value="" />
-<br />
-<strong>Serial: </strong>
-<input type="textarea" id="txtEmail1" name="txtEmail1" value="" />
-<br />
-<strong>Model: </strong>
-<input type="textarea" id="txtPhone1" name="txtPhone1" value="" />
-<br />
-<br />
-<a href="#" style="float: right; margin-right: 35px;" onClick="Add()">
-	<img src="<?php echo $root; ?>/images/add-button.png" border="0" title="Add" /></a>
-<a href="#" style="float: right; margin-right: 10px;" onClick="refreshPage()">
-	<img src="<?php echo $root; ?>/images/cancel-button.png" border="0" title="Cancel" /></a>
+}
+if ($addAccessory) { ?>
+<div id="showAddAccessory">
+<form id="frmAddAccessory" name="frmAddAccessory" enctype="multipart/form-data" action="actions-equipment.php" method="POST">
+	<label>Name: </label><input type="text" name="textfield" />
+	<label>Image: </label><input type="file" />
+	<label>Genre: </label><input type="text" name="textfield4" />
+	<label>Check Hours: </label><input type="text" name="textfield5" />
+	<label>Serial #: </label><input type="text" name="textfield6" />
+	<label>Model #: </label><input type="text" name="textfield7" />
+	<label>Thumbnail: </label><input type="file" />
+	<label>Needs Repair: </label><input type="checkbox" name="checkbox2" value="checkbox" /><label>Special Contract Required: </label><input type="checkbox" name="checkbox" value="checkbox" />
+	<label>Notes: </label><textarea name="textarea"></textarea>
+	<input type="hidden" name="MAX_FILE_SIZE" value="500" />
+	<br />
+	<a href="#" style="float: right; margin-right: 35px;" onClick="Add()">
+		<img src="<?php echo $root; ?>/images/add-button.png" border="0" title="Add" /></a>
+	<a href="#" style="float: right; margin-right: 10px;" onClick="refreshPage()">
+		<img src="<?php echo $root; ?>/images/cancel-button.png" border="0" title="Cancel" /></a>
 </form>
 </div>
 <?php
-}}
-?>
-<div style="position: absolute; bottom: 10px; left: 20px;"><a href="#" title="Add Student" onClick="showAddForm();"><img src="<?php echo $root; ?>/images/add-student-button.png" border="0" /></a></div>
-<?php
+}
+
 mysql_free_result($Equipment);
 ?>
