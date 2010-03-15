dad0c462022f7531cf6ba4b1a9ebce64b795a71c
wturnerharris <wturnerharris@2ce709ec-1cee-11df-959f-133e3f6a5118>
2010-03-15 04:43:07 +0000

    * new add class button * added the ability to add classes to the system * minor styling adjustments

--- equipment/admin/add-class.php
+++ equipment/admin/add-class.php
@@ -1,19 +1,26 @@
-<?php 
+<?php
 require_once('../config.php'); 
-
-$StudentID = $_POST['StudentID'];
-$class = $_POST['class'];
-$ClassID_query = "SELECT ID FROM class WHERE Name='$class'";
-mysql_select_db($database_equip, $equip);
-$classes = mysql_query($ClassID_query, $equip) or die(mysql_error());
-$option = mysql_fetch_array( $classes);
-$ClassID = $option['ID'];
-
-
-$sql = "INSERT INTO student_class(StudentID, ClassID) VALUES ('$StudentID','$ClassID')";
-//echo $sql;
-mysql_select_db($database_equip, $equip);
-mysql_query($sql, $equip) or die(mysql_error());
+$addNewClass = $_POST['addNewClass'];
+$Class = $_POST['txtClass'];
+if ($addNewClass == "true") {
+    //add new class
+    $sql = "INSERT INTO class (Name) VALUES ('$Class')";
+    mysql_select_db($database_equip, $equip);
+    mysql_query($sql, $equip) or die(mysql_error());
+} else {
+    //variables
+    $StudentID = $_POST['StudentID'];
+    $class = $_POST['class'];
+    //get class id
+    mysql_select_db($database_equip, $equip);
+    $ClassID_query = "SELECT ID FROM class WHERE Name='$class'";
+    $classes = mysql_query($ClassID_query, $equip) or die(mysql_error());
+    $option = mysql_fetch_array( $classes);
+    $ClassID = $option['ID'];
+    //add class per student id
+    $sql = "INSERT INTO student_class(StudentID, ClassID) VALUES ('$StudentID','$ClassID')";
+    mysql_select_db($database_equip, $equip);
+    mysql_query($sql, $equip) or die(mysql_error());
 
 ?>
 
@@ -25,5 +32,5 @@ StudentID: <? echo $StudentID ?><br />
 </div>
 
 <?
-include ('../studentinfo.php');
+include ('../studentinfo.php'); }
 ?>
--- equipment/admin/general.php
+++ equipment/admin/general.php
@@ -1,58 +1,13 @@
-<?php 
-
-//variables
-
-?>
-<script type="text/javascript">
-
-</script>		
-
 <pre>
 //options
-$checkHours = false; //to enable kit-specific due date change to true
-$dueToday = true; //only shows items due today on index page, false shows all
-$weekends = false; // if open weekends, flag this
-$dayClosed1 = "Sun"; // three-digit textual day, usually Sat
-$dayClosed2 = "Mon"; // three-digit textual day, usually Sun
+$checkHours = false; 
+//not kit-specific due date
+$dueToday = true; 
+//only shows items due today on index page
+$weekends = false;
+//if open weekends, flag this
+$dayClosed1 = "Sun"; 
+$dayClosed2 = "Mon";
+//three-digit textual day, usually Sat/Sun
 
-// due back BEFORE time on checkout
-$dueHours = "17:00:00"; // 24 hour time, with minutes and seconds 00:00:00
-
-// set your open and close times here. System will only work with Hour arguments, input hour in 24 hour time in format HH
-$monOpen = 00;
-$monClose = 00;
-$tueOpen = 14;
-$tueClose = 17;
-$wedOpen = 14;
-$wedClose = 17;
-$thuOpen = 14;
-$thuClose = 17;
-$friOpen = 14;
-$friClose = 17;
-$satOpen = 14;
-$satClose = 17;
-$sunOpen = 00;
-$sunClose = 00;
-
-//USE FINES OR STRIKES
-$fines = false; //3-strikes system default
-
-//$strikes = false;
-$maxStrike = 3;
-$gracePeriod = 1800; // grace period in seconds; for strikes. using fines ignores the $amountDue unless the full frequency time has passed.
-
-// fine controls
-$fineAmount = 1.00; // amount of fine
-$fineFreq = 86400; // amount of time per fine increment, in seconds
-$maxFine = 3.00; // max amount of fine per kit
-
-$root = "/equipment"; // using absolute paths. from the root to the program folder, with preceding slash and without trailing slash
-$site_root = "http://134.74.235.40"; //domain name where your installation of checkout resides, no trailing slash
-
-//admin email for error reporting
-$admin_email1 = "wturnerharris@gmail.com";
-$admin_email2 = "chase_a3@mac.com";
-$admin_email3 = "";
-
-//City College Note Specific
-$alert = "Do NOT add any students to any of the Photo Classes without permission from Chase Browder.";	<input id="filter" name="filter" type="hidden" value="<?php echo $filter; ?>" />
+for all options edit config.php
