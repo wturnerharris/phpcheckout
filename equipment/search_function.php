f6699141d82c782dd103d48b16f0e254a9b785ec
wturnerharris <wturnerharris@2ce709ec-1cee-11df-959f-133e3f6a5118>
2010-03-22 17:39:25 +0000

    * modified session_php.inc to correct footer/login sequence * removed obsolete search_function.php

--- equipment/search_function.php
+++
@@ -1,9 +0,0 @@
-<?PHP 
-
-$ServerKitID = $row_Recordset3['KitID'];
-mysql_select_db($database_kit, $equip);
-$query_Recordset5 = "SELECT * FROM checkedout WHERE KitID = $ServerKitID AND DateIn = ''";
-$Recordset5 = mysql_query($query_Recordset5, $equip) or die(mysql_error());
-$row_Recordset5 = mysql_fetch_assoc($Recordset5);
-$totalRows_Recordset5 = mysql_num_rows($Recordset5);
-?>
