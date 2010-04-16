9f3dbe8a246fc2359ed8eb867d386fc7642ec831
wturnerharris <wturnerharris@2ce709ec-1cee-11df-959f-133e3f6a5118>
2010-04-16 05:19:19 +0000

    * working on reservations

---
+++ trunk/cal/info.php
@@ -0,0 +1,120 @@
+<?php require_once('config.php');
+
+include('includes/heading.html'); 
+
+$KitID = $_REQUEST['KitID'];
+
+mysql_select_db($database_equip, $equip);
+$query_Recordset1 = sprintf("SELECT kit.ID AS KitID, kit.Name AS KitName, kit.Image AS KitImage, accessorytype.ID AS AccessoryTypeID, accessorytype.Name AS AccessoryTypeName, kit_accessorytype.ID AS KitAccID FROM kit LEFT JOIN kit_accessorytype ON kit_accessorytype.KitID = kit.ID LEFT JOIN accessorytype ON kit_accessorytype.AccessorytypeID = accessorytype.ID WHERE kit.ID = $KitID");
+$Recordset1 = mysql_query($query_Recordset1, $equip) or die(mysql_error());
+$row_Recordset1 = mysql_fetch_assoc($Recordset1);
+$totalRows_Recordset1 = mysql_num_rows($Recordset1);
+
+mysql_select_db($database_equip, $equip);
+$query_Recordset2 = sprintf("SELECT checkedout.ID AS checkID, Notes, LastName, FirstName, DateOut, DateIn, CheckoutUser, CheckinUser FROM checkedout LEFT JOIN students ON students.StudentID = checkedout.StudentID WHERE KitID = $KitID ORDER BY checkedout.ID DESC");
+$Recordset2 = mysql_query($query_Recordset2, $equip) or die(mysql_error());
+$row_Recordset2 = mysql_fetch_assoc($Recordset2);
+$totalRows_Recordset2 = mysql_num_rows($Recordset2);
+
+$CheckedOutCount = 0;
+?>
+
+<div align="center"><h1>Checkout History</h1>
+  <br>
+</div>
+<table width="600" border="0" align="center" cellpadding="3" cellspacing="3">
+
+
+<?php 
+$AccessoryCount = 0;
+
+do { 
+if ($CurrentKitID != $row_Recordset1['KitID']) {
+if ($FirstTime == 1) {
+echo "</td></tr>";
+}
+$AccessoryCount = 0;
+$AccessoryFirstTime = 0;
+
+?>
+<tr>
+    <td colspan="2"><strong>Equipment Name:</strong> <? echo $row_Recordset1['KitName']; ?></td>
+
+        </tr>
+        <tr>
+        <td valign='top' CLASS='accessoryText'>
+      <?
+if (isset($row_Recordset1['KitImage'])){
+echo "<p><img src='$root/images/".$row_Recordset1['KitImage']."' width='75%' height='75%' align='center'>";
+echo "</td><td valign='top' CLASS='accessoryText'>";
+}
+if (isset($row_Recordset1['AccessoryTypeName'])){
+echo '<em><strong>Accessories</strong></em>';
+}
+$CurrentKitID = $row_Recordset1['KitID'];
+echo "<br />";
+}
+if (isset($row_Recordset1['AccessoryTypeName'])){
+
+
+//echo $row_Recordset1['KitAccID'];
+//echo " - ";
+if($AccessoryCount > 8){
+if($AccessoryFirstTime < 1){
+echo "</td><td valign='top' CLASS='accessoryText'>";
+$AccessoryFirstTime++;
+}
+}
+echo $row_Recordset1['AccessoryTypeName'];
+echo "<BR>";
+$AccessoryCount++;
+}
+
+$FirstTime++;
+ } while ($row_Recordset1 = mysql_fetch_assoc($Recordset1)); 
+ ?>
+        </div>
+      </blockquote>
+</table>
+<h2><blockquote>History Detail:</blockquote></h2>
+<table width="600" border="0" align="center" cellpadding="3" cellspacing="3">
+	<tr>
+		<td><strong>Student Name</strong></td>
+		<td><strong>Date Out</strong></td>
+		<td><strong>By</strong></td>
+		<td><strong>Date In</strong></td>
+		<td><strong>By</strong></td>
+	</tr>
+	
+		
+        <?
+       
+       if(isset($row_Recordset2['checkID'])){
+    
+        do { 
+        if($row_Recordset2['checkID']!=$tempID){
+        ?>
+        <tr>
+        	<td><? echo $row_Recordset2['LastName'] ?>, <? echo $row_Recordset2['FirstName'] ?></td>
+        	<td><? echo $row_Recordset2['DateOut'] ?></td>
+        	<td><? echo $row_Recordset2['CheckoutUser'] ?></td>
+        	<td><? echo $row_Recordset2['DateIn'] ?></td>
+        	<td><? echo $row_Recordset2['CheckinUser'] ?></td>
+        </tr>
+        <tr>
+        	<td colspan="5"><? echo $row_Recordset2['Notes'] ?></td>
+        </tr>
+        
+     
+        
+   <?	
+  }
+  $tempID=$row_Recordset2['checkID'];
+         } while ($row_Recordset2 = mysql_fetch_assoc($Recordset2));
+ 
+       }
+        ?>
+</table>
+<p>Back to <a href="allequipmentbyclass.php">Equipment</a><br />
+
+<? include('includes/footer.html');  ?>
---
+++ trunk/cal/reserve_action.php
@@ -0,0 +1,158 @@
+<?php 
+
+require_once('config.php'); 
+
+$CheckoutUser = $_REQUEST['User'];
+$KitName = $_REQUEST['KitName'];
+$ReturnDate = $_POST['ReturnDate'];
+$FirstName = $_REQUEST['FirstName'];
+$LastName = $_REQUEST['LastName'];
+$CheckoutDate = time();
+$StudentID = $_REQUEST['StudentID'];
+$KitID = $_REQUEST['KitID'];
+$Accessories = $_REQUEST['Accessories'];
+$Notes = $_REQUEST['Notes'];
+
+if ($_REQUEST['ContractRequired']==1) {
+
+	require_once('contract.txt'); 
+?>
+	
+	<P>
+	<b>
+	Kit: <i><? echo $KitName ?></i> 
+	<P>
+	<?
+	$i = 0;
+	$j = 0;
+	
+	do { 
+	$AccessoryID = "Accessory$i";
+	
+	if ($_REQUEST[$AccessoryID] != "") {
+	if ($j > 0) {
+	$Accessories = $Accessories . ", " . $_REQUEST[$AccessoryID]; 
+	} else {
+	$Accessories = $_REQUEST[$AccessoryID]; 
+	$j++;
+	}
+	}
+	
+	$i++;
+	} while ($i < 50); 
+	
+	
+	?>
+	Accessories: <i><? echo $Accessories ?></i>
+	
+	<P>
+	Notes: <i><? echo $_REQUEST['Notes']; ?></i>
+	<P>
+	Checked Out: <i><? echo date("D, F j, g:i a"); ?></i>
+	<P>
+	Due Back: <i><? echo date("D, F j, g:i a", strtotime($_REQUEST['ReturnDate'])); ?></i><br>
+	<P>
+	<P>
+	Student's Signature: 
+	
+	</div>
+	</td>
+	</tr>
+	</table>
+	</center>
+	
+	
+	<form name="frmCheckOut" action="checkoutaction.php" method="post">
+	
+	<input type="hidden" name="FirstName" value="<? echo $FirstName; ?>">
+	<input type="hidden" name="LastName" value="<? echo $LastName; ?>">
+	<input type="hidden" name="KitName" value="<? echo $KitName; ?>">
+	<input type="hidden" name="KitID" value="<? echo $KitID; ?>">
+	<input type="hidden" name="StudentID" value="<? echo $StudentID; ?>">
+	<input type="hidden" name="ReturnDate" value="<? echo $ReturnDate; ?>">
+	<input type="hidden" name="Accessories" value="<? echo $Accessories; ?>">
+	<input type="hidden" name="Notes" value="<? echo $Notes; ?>">
+	<P>
+	<center>
+	<input type="submit" name="Submit" value="Contract Signed and Filed">
+	</center>
+
+<?
+} else {
+include('includes/heading.html');
+
+function multi_post_item($repeatedString) {
+$ArrayOfItems = array();
+$raw_input_items = split("&", $_SERVER['QUERY_STRING']);
+foreach($raw_input_items as $input_item) {
+$itemPair = split("=", $input_item);
+if($itemPair[0]==$repeatedString){
+$ArrayOfItems[]=$itemPair[1];
+}
+}
+return $ArrayOfItems;
+}
+
+//$AccArrray = multi_post_item($HTTP_GET_VARS['Accessory_TypeID']);
+//echo $AccArrray[0];
+//echo $Accessory_TypeID[0];
+//echo $Accessory_TypeID[1];
+//echo "<P>";
+//echo var_dump($HTTP_GET_VARS['Accessory_TypeID']);
+
+
+
+
+do { 
+$AccessoryID = "Accessory$i";
+
+if ($_REQUEST[$AccessoryID] != "") {
+if ($j > 0) {
+$Accessories = $Accessories . ", " . $_REQUEST[$AccessoryID]; 
+} else {
+$Accessories = $_REQUEST[$AccessoryID]; 
+$j++;
+}
+}
+
+$i++;
+} while ($i < 50); 
+
+
+//$CheckoutUser = $HTTP_COOKIE_VARS["EquipmentCheckout"];
+$sql = "INSERT INTO checkedout (ID , KitID , StudentID , DateOut , ExpectedDateIn , DateIn , FinePaid , BannedDate, Accessories, Notes, CheckoutUser) VALUES ('', '$KitID', '$StudentID', NOW(), '$ReturnDate', '', NULL , NULL, '$Accessories', '$Notes', '$CheckoutUser');";
+//echo $sql;
+mysql_select_db($database_equip, $equip);
+mysql_query($sql, $equip) or die(mysql_error());
+//$Recordset1 = mysql_query($sql, $equip) or die(mysql_error());
+//$row_Recordset1 = mysql_fetch_assoc($Recordset1);
+//$totalRows_Recordset1 = mysql_num_rows($Recordset1);
+
+?>
+<meta http-equiv="refresh" content="10;URL=studentinfo.php?StudentID=<? echo $StudentID; ?>">
+
+<center><h1>Item Checked Out</h1></center>
+<strong><h2>Summary</h2></strong>
+<br>
+<div id="tag-br">
+	<div id="tag-top"><?php echo $FirstName; ?> <?php echo $LastName; ?></div>
+	<div id="tag-info"> has checked out the <?php echo $KitName; ?>.</div>
+</div>
+<div id="tag-br">
+	<div id="options">With the following accessories: <? if ($Accessories != ""){ echo $Accessories; } else { echo "N/A"; } ?><br/></div>
+</div>
+<div id="tag-br">
+	<div id="tag">Beginning:</div>
+	<div id="tag-info"><? echo date("D, F j, g:i a"); ?></div>
+</div>
+<div id="tag-br">
+	<div id="tag">Due Back:</div>
+	<div id="tag-info"><span id="changeDate"><i class="alert"><? echo date("D, F j", strtotime($_REQUEST['ReturnDate'])); ?></i></span>, BEFORE <? echo date("g:i a", strtotime($_REQUEST['ReturnDate']));?></div>
+</div>
+
+<p><em>Please print now for a receipt. Otherwise this page will refresh automatically in 10 seconds. </em></p><br/>
+Return to <a href="studentinfo.php?StudentID=<? echo $StudentID; ?>">Student Info Page</a><br />
+
+<? include('includes/footer.html'); 
+}
+?>
