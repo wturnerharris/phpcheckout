b76a7f8aa9bd0191688cc258fdac23b4d696bbfb
wturnerharris <wturnerharris@2ce709ec-1cee-11df-959f-133e3f6a5118>
2010-04-19 08:49:16 +0000

    * new page to list reservations

---
+++ equipment/reserved.php
@@ -0,0 +1,60 @@
+<?php
+require_once('config.php');
+include('includes/heading.html'); 
+
+mysql_select_db($database_equip, $equip);
+$query_Recordset1 = "SELECT * FROM checkedout LEFT JOIN students ON students.StudentID = checkedout.StudentID LEFT JOIN kit ON kit.ID = checkedout.KitID WHERE DateIn = '' AND ReserveDate >= CURDATE() ORDER BY checkedout.ExpectedDateIn";
+$Recordset1 = mysql_query($query_Recordset1, $equip) or die(mysql_error());
+$row_Recordset1 = mysql_fetch_assoc($Recordset1);
+$totalRows_Recordset1 = mysql_num_rows($Recordset1);
+
+?>
+
+
+
+  <?
+if($row_Recordset1['ID']>0) {  
+
+echo "<center><strong><h1>Currently Reserved</h1></strong></center><p>";
+
+do { ?>
+<?
+$StudentID = $row_Recordset1['StudentID'];
+mysql_select_db($database_kit, $equip);
+$query_Recordset4 = sprintf("SELECT kit.ID AS KitID, checkedout.ID AS CheckOutID, kit.Name, checkedout.DateOut, checkedout.ExpectedDateIn FROM checkedout INNER JOIN kit ON kit.ID = checkedout.KitID WHERE checkedout.DateIn = '' AND ReserveDate >= CURDATE() AND StudentID = \"$StudentID\"");
+//echo $query_Recordset4 ;
+$Recordset4 = mysql_query($query_Recordset4, $equip) or die(mysql_error());
+$row_Recordset4 = mysql_fetch_assoc($Recordset4);
+$totalRows_Recordset4 = mysql_num_rows($Recordset4);
+?>
+	<strong>Equipment Reserved:</strong> <?php echo $row_Recordset1['Name']; ?><BR> 
+	<strong>Student Name: </strong><?php echo $row_Recordset1['FirstName']; ?> <?php echo $row_Recordset1['LastName']; ?><br>
+	<strong>Student ID: </strong><a href="studentinfo.php?StudentID=<?php echo $row_Recordset1['StudentID']; ?>"><?php echo $row_Recordset1['StudentID']; ?></a><br>
+	<strong>Email: </strong><?php echo $row_Recordset1['Email']; ?><br>
+	<strong>Reservation: </strong><a href="checkin.php?CheckedOutID=<?php echo $row_Recordset4['CheckOutID']; ?>&KitID=<?php echo $row_Recordset4['KitID']; ?>&StudentID=<?php echo $row_Recordset1['StudentID']; ?>">Check Item Out</a> | <a href="renew.php?CheckedOutID=<?php echo $row_Recordset4['CheckOutID']; ?>&KitID=<?php echo $row_Recordset4['KitID']; ?>&StudentID=<?php echo $row_Recordset1['StudentID']; ?>" title="Renew">Cancel</a><br>
+    <?php 
+echo '<strong>Reservation Date: </strong>';
+//echo $row_Recordset1['DateOut'];
+//echo strtotime($row_Recordset1['DateOut']);
+echo date("D, F j, g:i a", strtotime($row_Recordset1['DateOut'])); 
+echo '<br>';
+echo '<strong>Expected Return Date: </strong>'; 
+
+
+echo date("D, F j, g:i a", strtotime($row_Recordset1['ExpectedDateIn'])); 
+if (intval(strtotime($row_Recordset1['ExpectedDateIn'])) < intval(strtotime("now"))) {
+echo "<strong><font color=\"#FF0000\"> -- THIS ITEM IS LATE</font></strong>";
+}
+echo '<P>';
+echo '<hr />';
+
+ } while ($row_Recordset1 = mysql_fetch_assoc($Recordset1)); 
+ } else {
+ 
+echo "<center><strong><h1>All Equipment Checked In</h1></strong></center><p>";
+ }
+
+mysql_free_result($Recordset1);
+include('includes/footer.html'); 
+?>
+
