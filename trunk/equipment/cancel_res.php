811b38ff3f08fe800ecc42a79fe252d32c3ebf72
wturnerharris <wturnerharris@2ce709ec-1cee-11df-959f-133e3f6a5118>
2010-04-23 00:52:12 +0000

    * minor bug fixes * added reservation cancel code

---
+++ trunk/equipment/cancel_res.php
@@ -0,0 +1,26 @@
+<?php
+require_once('config.php'); 
+
+$CheckOutID = $_REQUEST['CheckOutID'];
+$sid = $_REQUEST['StudentID'];
+mysql_select_db($database_equip, $equip);
+$remove = "SELECT * FROM checkedout WHERE checkedout.ID='$CheckOutID' AND StudentID='$sid'";
+$Recordset1 = mysql_query($remove, $equip) or die(mysql_error());
+$row_check = mysql_fetch_assoc($Recordset1);
+$totalRows_Recordset1 = mysql_num_rows($Recordset1);
+
+include('reserved.php'); 
+echo "<div id='overlay'></div>";
+echo "<div id='alert' class='alert' style='visibility: visible;'>";
+
+if ($row_check['StudentID'] == $sid) {
+	mysql_select_db($database_equip, $equip);
+	$query_Recordset2 = "DELETE FROM checkedout WHERE checkedout.ID='$CheckOutID' AND StudentID='$sid'";
+	mysql_query($query_Recordset2, $equip) or die(mysql_error());
+	echo "Reservation Canceled.</div>";
+	echo "<meta http-equiv='refresh' content='3;URL=studentinfo.php?StudentID=$sid'>";
+} else {
+	echo "Reservations should be canceled from the Reservation page or under the student who reserved it.</div>";
+	echo "<meta http-equiv='refresh' content='3;URL=reserved.php'>";
+}
+?>
