44f270e705b0d2ebf5a49097cb7ab40ef9b5ff5d
wturnerharris@gmail.com <wturnerharris@gmail.com@2ce709ec-1cee-11df-959f-133e3f6a5118>
2011-02-20 19:41:17 +0000

    * fixed uid not being updated on modify-student.php

--- trunk/equipment/admin/modify-student.php
+++ trunk/equipment/admin/modify-student.php
@@ -6,9 +6,10 @@ $FirstName = $_POST['txtFirstName'];
 $LastName = $_POST['txtLastName'];
 $Email = $_POST['txtEmail'];
 $Phone = $_POST['txtPhone'];
+$UserID = $_POST['txtUser'];
 $pID = $_POST['pID'];
 
-$modify = "UPDATE students SET StudentID='$StudentID', FirstName='$FirstName', LastName='$LastName', Email='$Email', Phone='$Phone' WHERE ID='$pID'";
+$modify = "UPDATE students SET StudentID='$StudentID', FirstName='$FirstName', LastName='$LastName', Email='$Email', Phone='$Phone', UID='$UserID' WHERE ID='$pID'";
 mysql_select_db($database_equip, $equip);
 mysql_query($modify, $equip) or die(mysql_error());
 
